2064ed65de306872fc48d4eda71bff2e0d7e7e68
hoangminhbao8102 <90952548+hoangminhbao8102@users.noreply.github.com>
2023-09-19 00:03:10 +0700

    main

---
+++ BaiTapLab/Lab01/HuongDan/Lab01_Bai01_HelloWorld/Lab01_Bai01_HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab01_Bai01_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("Hello, ");
+            Console.Write("World!");
+            Console.WriteLine();
+            Console.WriteLine("Hello, " + "World!");
+            Console.WriteLine("Da xuat chuoi Hello, World! " + 3 + " lan roi");
+            Console.ReadKey();
+        }
+    }
+}
