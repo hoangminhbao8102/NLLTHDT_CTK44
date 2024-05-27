1e05158414891e82695b47d05e4dff9e15b51f2a
hoangminhbao8102 <90952548+hoangminhbao8102@users.noreply.github.com>
2024-05-27 08:25:01 +0700

    main

---
+++ BaiTapLab/Lab02/BatBuoc/Lab02_D_Bai01/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02_D_Bai01
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.Write("Nhập chiều dài: ");
+            double d = double.Parse(Console.ReadLine());
+
+            Console.Write("Nhập chiều rộng: ");
+            double r = double.Parse(Console.ReadLine());
+
+            Console.Write("Nhập chiều cao: ");
+            double h = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Tổng diện tích mặt: " + TongDienTichMat(d, r, h));
+            Console.WriteLine("Thể tích: " + TheTich(d, r, h));
+
+            Console.ReadKey();
+        }
+
+        public static double TongDienTichMat(double d, double r, double h)
+        {
+            return 2 * (d * r + r * h + h * d);
+        }
+
+        public static double TheTich(double d, double r, double h) 
+        {
+            return d * r * h;
+        }
+    }
+}
